5e227751291a4e29bebdaf85d41e704ca903b6e5
Robin Wakeman <robin.e.wakeman@gmail.com>
2019-11-05 01:21:33 -0500

    front end project setup

---
+++ theme.js
@@ -0,0 +1,32 @@
+import { createMuiTheme } from '@material-ui/core/styles';
+import { red } from '@material-ui/core/colors';
+
+// Crate a theme instance.
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#556cd6',
+    },
+    secondary: {
+      main: '#19857b',
+    },
+    error: {
+      main: red.A400,
+    },
+    background:{
+      default: '#e3e3e3',
+      white: '#ffffff,'
+    },
+  },
+  typography: {
+    h4: {
+      fontSize: 16,
+      fontWeight: 500,
+    },
+    body1: {
+      fontSize: 14,
+    },
+  },
+});
+
+export default theme;
